5a145827ff998531013fd9e695cb911a7c6dcb1e
Marcin Puc <marcin.e.puc@gmail.com>
2021-11-18 18:29:40 +0100

    Add measure_and_print function

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,30 @@ mod bufwrap;
 
 pub use args::BufferedInput;
 pub use elapsed::measure_time;
+
+use std::fmt::Display;
+
+/// Runs the code, and prints out the elapsed time and the result.
+///
+/// First prints the elapsed time to stderr,
+/// then the result to stdout, in separate lines.
+///
+/// # Example
+///
+/// ```
+/// use aoc_utils::measure_and_print;
+///
+/// measure_and_print(|| {
+///     (0..10_000).sum::<u64>()
+/// });
+///
+/// // Prints:
+/// // 227.81 μs
+/// // 49995000
+/// ```
+pub fn measure_and_print<T: Display, F: FnOnce() -> T>(f: F) {
+    let (elapsed, result) = elapsed::measure_time(f);
+
+    eprintln!("{}", elapsed);
+    println!("{}", result);
+}
